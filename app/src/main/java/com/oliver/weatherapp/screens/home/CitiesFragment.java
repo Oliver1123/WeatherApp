c768d65e917b51f48cb0f448fcee28c423e90fd7
Oleg Zolotar <zolotar1123@gmail.com>
2018-05-10 19:07:15 +0300

    add new place selection

--- app/src/main/java/com/oliver/weatherapp/screens/home/CitiesFragment.java
+++ app/src/main/java/com/oliver/weatherapp/screens/home/CitiesFragment.java
@@ -1,18 +1,29 @@
 package com.oliver.weatherapp.screens.home;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
 import com.oliver.weatherapp.R;
 
+import static android.app.Activity.RESULT_OK;
+
 public class CitiesFragment extends Fragment {
+
+    private static final int PLACE_PICKER_REQUEST = 111;
+    private static final String TAG = CitiesFragment.class.getSimpleName();
 
     public static CitiesFragment newInstance() {
 
@@ -44,6 +55,31 @@ public class CitiesFragment extends Fragment {
     }
 
     private void addCityClick(View view) {
-        Snackbar.make(view, "Hello Snackbar", Snackbar.LENGTH_SHORT).show();
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try {
+            startActivityForResult(builder.build(getActivity()), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(getActivity(), data);
+                handleSelectedPlace(place);
+
+            }
+        }
+    }
+
+    private void handleSelectedPlace(Place place) {
+        CharSequence name = place.getName();
+        LatLng latLng = place.getLatLng();
+        Log.d(TAG, "handleSelectedPlace: name: " + name + " latlng: " + latLng);
     }
 }
