3ea2061eb28f1142c650aa30de0e24b2162dba70
Oleg Zolotar <zolotar1123@gmail.com>
2018-05-11 10:58:20 +0300

    show snackbar after place is saved

--- app/src/main/java/com/oliver/weatherapp/screens/home/CitiesFragment.java
+++ app/src/main/java/com/oliver/weatherapp/screens/home/CitiesFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
@@ -155,6 +156,8 @@ public class CitiesFragment extends Fragment {
 
         CityEntry cityEntry = new CityEntry(name, address, latLng.latitude, latLng. longitude);
         mViewModel.addCity(cityEntry);
+
+        Snackbar.make(getView(), R.string.msg_place_saved, Snackbar.LENGTH_SHORT).show();
     }
 
     private void onCitiesUpdated(List<CityEntry> cities) {
