1774bf3d359b665144f7d1786bb6bba4dfa0683c
Oleg Zolotar <zolotar1123@gmail.com>
2018-05-12 09:09:50 +0300

    create blank help fragment

--- app/src/main/java/com/oliver/weatherapp/screens/MainActivity.java
+++ app/src/main/java/com/oliver/weatherapp/screens/MainActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.oliver.weatherapp.R;
 import com.oliver.weatherapp.data.local.model.CityEntry;
 import com.oliver.weatherapp.screens.forecast.WeatherFragment;
+import com.oliver.weatherapp.screens.help.HelpFragment;
 import com.oliver.weatherapp.screens.home.CitiesFragment;
 
 public class MainActivity extends AppCompatActivity implements CitiesFragment.CitiesFragmentInteractionListener {
@@ -41,5 +45,33 @@ public class MainActivity extends AppCompatActivity implements CitiesFragment.Ci
     @Override
     public void onCitySelected(CityEntry city) {
         setFragment(WeatherFragment.newInstance(city), true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_help) {
+            showHelpFragment();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showHelpFragment() {
+        if (!TextUtils.equals(HelpFragment.class.getSimpleName(), getCurrentFragmentName())) {
+            setFragment(HelpFragment.newInstance(), true);
+        }
+    }
+
+    private String getCurrentFragmentName() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
+        if (fragment != null)
+            return fragment.getClass().getSimpleName();
+
+        return null;
     }
 }
---
+++ app/src/main/java/com/oliver/weatherapp/screens/help/HelpFragment.java
@@ -0,0 +1,31 @@
+package com.oliver.weatherapp.screens.help;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.oliver.weatherapp.R;
+
+public class HelpFragment extends Fragment {
+
+
+    public static Fragment newInstance() {
+        return new HelpFragment();
+    }
+
+    public HelpFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_help, container, false);
+    }
+
+}
