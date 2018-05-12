81fce035c6946f1541acd3d9d1718f4dc0bcefbd
Oleg Zolotar <zolotar1123@gmail.com>
2018-05-12 09:33:23 +0300

    fix lat-lng bug

--- app/src/main/java/com/oliver/weatherapp/screens/forecast/WeatherViewModel.java
+++ app/src/main/java/com/oliver/weatherapp/screens/forecast/WeatherViewModel.java
@@ -19,7 +19,7 @@ public class WeatherViewModel extends ViewModel {
     public WeatherViewModel(WeatherRepository repository, CityEntry city) {
         Log.d(TAG, "WeatherViewModel: constructor repository: " + repository);
         mRepository = repository;
-        mForecast = mRepository.getForecast(city.id, city.longitude, city.latitude);
+        mForecast = mRepository.getForecast(city.id, city.latitude, city.longitude);
     }
 
     public LiveData<List<WeatherEntry>> getForecast() {
